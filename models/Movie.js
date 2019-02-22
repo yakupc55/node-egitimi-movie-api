092beb67412d1a37868897fe0e89df765ba65058
unknown <yakupceran00@gmail.com>
2019-02-22 15:49:23 +0300

    validation movie

--- models/Movie.js
+++ models/Movie.js
@@ -5,12 +5,31 @@ const MovieSchema = new Schema({
     director_id: Schema.Types.ObjectId,
    title:{
        type: String,
-        required : true,
+        required : [true,'`{PATH}` alanı zorunludur.'],
+       maxlength : [15, '`{PATH}` alanı `{MAXLENGTH}`  karekterden uzun olamaz. siz `{VALUE}` karekter girdiniz'],
+       minlength: [4, '`{PATH}` alanı `{MINLENGTH}`  karekterden kısa olamaz. siz `{VALUE}` karekter girdiniz']
+
    },
-    category: String,
-    country : String,
-    year: Number,
-    imdb_score: Number,
+    category:{
+        type : String,
+        maxlength : 30,
+        minlength:1
+    } ,
+    country : {
+        type : String,
+        maxlength : 30,
+        minlength:1
+    },
+    year: {
+        type : Number,
+        maxlength : 2040,
+        minlength:1900
+    },
+    imdb_score: {
+        type : Number,
+        maxlength : 10,
+        minlength:0
+    },
     createdAt: {
         type: Date,
         default: Date.now
