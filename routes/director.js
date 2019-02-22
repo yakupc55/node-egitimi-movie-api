fbd01992ccee2c624cb62ab72249e2af890d5563
unknown <yakupceran00@gmail.com>
2019-02-22 14:11:16 +0300

    director id and list

--- routes/director.js
+++ routes/director.js
@@ -1,11 +1,10 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
 //Models
 const Director = require('../models/Director');
-router.get('/', (req, res)=> {
-  res.json({title : 'Express'});
-});
+
 
 router.post('/', (req, res,next)=> {
     const director = new Director(req.body);
@@ -17,6 +16,98 @@ router.post('/', (req, res,next)=> {
     });
 });
 
+router.get('/', (req, res)=> {
+   const promise = Director.aggregate([
+   {   $lookup : {
+           from: 'movies',
+           localField: '_id',
+           foreignField: 'director_id',
+           as: 'movies'
+       }
+   },
+       {
+           $unwind: {
+               path : '$movies',
+               preserveNullAndEmptyArrays: true
+           }
+       },
+       {//gruplama yapmamızı sağlayan yapı
+           $group:{
+               _id:{
+                   _id: '$_di',
+                   name: '$name',
+                   surname: '$surname',
+                   bio: '$bio',
+               },
+               movies:{
+                   $push: '$movies'
+               }
+           }
+       },
+       {//listelemenin nasıl yapılacağını burda yapıyoruz
+           $project: {
+               _id: '$_id._id',
+               name: '$_id.name',
+               surname: '$_id.surname',
+                movies: '$movies'
+           }
+       }
+   ]);
+   promise.then((data)=>{
+       res.json(data);
+   }).catch((err)=>{
+        res.json(err);
+   });
+});
+
+router.get('/:director_id', (req, res)=> {
+    const promise = Director.aggregate([
+        {
+          $match: {
+              '_id' : mongoose.Types.ObjectId(req.params.director_id)
+          }
+        },
+        {   $lookup : {
+                from: 'movies',
+                localField: '_id',
+                foreignField: 'director_id',
+                as: 'movies'
+            }
+        },
+        {
+            $unwind: {
+                path : '$movies',
+                preserveNullAndEmptyArrays: true
+            }
+        },
+        {//gruplama yapmamızı sağlayan yapı
+            $group:{
+                _id:{
+                    _id: '$_di',
+                    name: '$name',
+                    surname: '$surname',
+                    bio: '$bio',
+                },
+                movies:{
+                    $push: '$movies'
+                }
+            }
+        },
+        {//listelemenin nasıl yapılacağını burda yapıyoruz
+            $project: {
+                _id: '$_id._id',
+                name: '$_id.name',
+                surname: '$_id.surname',
+                movies: '$movies'
+            }
+        }
+    ]);
+    promise.then((data)=>{
+        res.json(data);
+    }).catch((err)=>{
+        res.json(err);
+    });
+});
 
 
 module.exports = router;
